abdb83a191c2e3ff49d2518c6d42ae3068dd3ff1
evan.al4d <evan.al4d@gmail.com>
2025-03-07 15:13:32 -0600

    update JS Index 1

---
+++ pp_js/index.js
@@ -0,0 +1,17 @@
+function page_load(){
+    const msgText = "#IndexPage: Page Load"
+
+    console.log(msgText.toUpperCase())
+}
+
+function giphyApi1()
+{
+    let msgText = "IndexPage: giphyApi1"
+
+    console.log(msgText.toUpperCase())
+    const divdisplayinfo = document.getElementById("userInfoDisplay");
+}
+//Api Key- I0M7oqswqY3bJpKrXBqcdUlBaasYa4Dd
+//APi link- https://api.giphy.com/v1/gifs/search?api_key=I0M7oqswqY3bJpKrXBqcdUlBaasYa4Dd&q=&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips
+
+ 
